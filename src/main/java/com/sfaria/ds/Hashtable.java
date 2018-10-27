f59cb88036de886cabce2c0d5f1a538297a5779e
Scott Faria <scott.faria@gmail.com>
2018-10-27 18:34:29 +0200

    hashtable done

--- src/main/java/com/sfaria/ds/Hashtable.java
+++ src/main/java/com/sfaria/ds/Hashtable.java
@@ -1,5 +1,7 @@
 package com.sfaria.ds;
 
+import java.util.Objects;
+
 /**
  * @author Scott Faria (scott.faria@gmail.com)
  */
@@ -8,7 +10,9 @@ public final class Hashtable<K, V> {
     // -------------------- Private Variables --------------------
 
     private final float loadFactor;
-    private Entry<?, ?>[] buckets;
+
+    private int itemCount;
+    private Entry<?, ?>[] entries;
 
     // -------------------- Constructors --------------------
 
@@ -21,37 +25,115 @@ public final class Hashtable<K, V> {
             throw new IllegalArgumentException(String.format("Load factor is too low or invalid: %d", loadFactor));
         }
         this.loadFactor = loadFactor;
-        this.buckets = new Entry<?, ?>[initialCapacity];
+        this.itemCount = 0;
+        this.entries = new Entry<?, ?>[initialCapacity];
     }
 
     // -------------------- Public Methods --------------------
 
+    public final int size() {
+        return itemCount;
+    }
+
+    @SuppressWarnings("unchecked")
     public final V put(K key, V value) {
+        Objects.requireNonNull(key);
+        Objects.requireNonNull(value);
+
+        int index = (key.hashCode() & 0x7FFFFFFF) % entries.length;
+        Entry<K, V> entry = (Entry<K, V>) entries[index];
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                //noinspection unchecked
+                V oldValue = (V) entry.value;
+                entry.value = value;
+                return oldValue;
+            }
+            entry = entry.next;
+        }
+
+        itemCount++;
+        rehash();
+        index = (key.hashCode() & 0x7FFFFFFF) % entries.length;
+        if (entries[index] != null) {
+            Entry<K, V> newEntry = new Entry<>(key, value, ((Entry<K, V>) entries[index]));
+            entries[index] = newEntry;
+        } else {
+            entries[index] = new Entry<>(key, value);
+        }
         return null;
     }
 
+    @SuppressWarnings("unchecked")
     public final V get(K key) {
+        Objects.requireNonNull(key);
+        int index = (key.hashCode() & 0x7FFFFFFF) % entries.length;
+        Entry<?, ?> entry = entries[index];
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                return (V) entry.value;
+            }
+            entry = entry.next;
+        }
         return null;
     }
 
+    @SuppressWarnings("unchecked")
     public final V remove(K key) {
+        Objects.requireNonNull(key);
+        Objects.requireNonNull(key);
+        int index = (key.hashCode() & 0x7FFFFFFF) % entries.length;
+        Entry<K, V> entry = (Entry<K, V>) entries[index];
+        Entry<K, V> previous = null;
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                V value = entry.value;
+                if (previous == null) {
+                    entries[index] = entry.next;
+                } else {
+                    previous.next = entry.next;
+                }
+                itemCount--;
+                return value;
+            }
+            previous = entry;
+            entry = entry.next;
+        }
         return null;
-    }
-
-    public final boolean containsKey(K key) {
-        return false;
     }
 
     // -------------------- Private Methods --------------------
+
+    @SuppressWarnings("unchecked")
+    private void rehash() {
+        if ((itemCount * loadFactor) > entries.length) {
+            Entry<?, ?>[] newEntries = new Entry<?, ?>[entries.length * 2];
+            for (Entry<?, ?> entry : entries) {
+                if (entry != null) {
+                    int newIndex = (entry.key.hashCode() & 0x7FFFFFFF) % newEntries.length;
+                    Entry<K, V> extantEntry = (Entry<K, V>) newEntries[newIndex];
+                    if (extantEntry != null) {
+                        while (extantEntry.next != null) {
+                            extantEntry = extantEntry.next;
+                        }
+                        extantEntry.next = (Entry<K, V>) entry;
+                    } else {
+                        newEntries[newIndex] = entry;
+                    }
+                }
+            }
+            entries = newEntries;
+        }
+    }
 
     // -------------------- Inner Classes --------------------
 
     private static final class Entry<K, V> {
         private final K key;
-        private final V value;
-        private final Entry<K, V> next;
+        private V value;
+        private Entry<K, V> next;
 
-        public Entry(K key, V value) {
+        Entry(K key, V value) {
             this(key, value, null);
         }
 
--- src/test/java/com/sfaria/ds/HashtableTest.java
+++ src/test/java/com/sfaria/ds/HashtableTest.java
@@ -2,6 +2,8 @@ package com.sfaria.ds;
 
 import org.junit.Test;
 
+import java.util.Objects;
+
 import static org.junit.Assert.*;
 
 /**
@@ -12,18 +14,147 @@ public class HashtableTest {
     // -------------------- Test Methods --------------------
 
     @Test
-    public void put() {
+    public void test_get_put_no_collisions() {
+        Hashtable<String, String> table = new Hashtable<>(10, 0.7f);
+        assertNull(table.put("A", "B"));
+        assertNull(table.put("B", "C"));
+        assertNull(table.put("C", "D"));
+        assertNull(table.put("E", "F"));
+
+        assertEquals("B", table.get("A"));
+        assertEquals("C", table.get("B"));
+        assertEquals("D", table.get("C"));
+        assertEquals("F", table.get("E"));
+        assertNull(table.get("F"));
+
+        assertEquals("B", table.put("A", "G"));
+        assertEquals("G", table.get("A"));
     }
 
     @Test
-    public void get() {
+    public void test_put_rehash() {
+        Hashtable<String, String> table = new Hashtable<>(1, 0.7f);
+        assertNull(table.put("A", "B"));
+        assertNull(table.put("B", "C"));
+        assertNull(table.put("C", "D"));
+        assertNull(table.put("E", "F"));
+
+        assertEquals("B", table.get("A"));
+        assertEquals("C", table.get("B"));
+        assertEquals("D", table.get("C"));
+        assertEquals("F", table.get("E"));
+        assertNull(table.get("F"));
+
+        assertEquals("B", table.put("A", "G"));
+        assertEquals("G", table.get("A"));
     }
 
     @Test
-    public void remove() {
+    public void test_get_put_with_guaranteed_collisions() {
+        Hashtable<CollidingKey, String> table = new Hashtable<>(1, 0.7f);
+        assertNull(table.put(new CollidingKey("A"), "B"));
+        assertNull(table.put(new CollidingKey("B"), "C"));
+        assertNull(table.put(new CollidingKey("C"), "D"));
+        assertNull(table.put(new CollidingKey("E"), "F"));
+
+        assertEquals("B", table.get(new CollidingKey("A")));
+        assertEquals("C", table.get(new CollidingKey("B")));
+        assertEquals("D", table.get(new CollidingKey("C")));
+        assertEquals("F", table.get(new CollidingKey("E")));
+        assertNull(table.get(new CollidingKey("F")));
     }
 
     @Test
-    public void containsKey() {
+    public void test_remove_no_collisions() {
+        Hashtable<String, String> table = new Hashtable<>(10, 0.7f);
+        assertNull(table.put("A", "B"));
+        assertNull(table.put("B", "C"));
+        assertNull(table.put("C", "D"));
+        assertNull(table.put("E", "F"));
+
+        assertEquals("B", table.remove("A"));
+        assertEquals("C", table.remove("B"));
+        assertEquals("D", table.remove("C"));
+        assertEquals("F", table.remove("E"));
+        assertNull(table.remove("A"));
+        assertNull(table.remove("X"));
+    }
+
+    @Test
+    public void test_remove_with_collisions() {
+        Hashtable<CollidingKey, String> table = new Hashtable<>(1, 0.7f);
+        assertNull(table.put(new CollidingKey("A"), "B"));
+        assertNull(table.put(new CollidingKey("B"), "C"));
+        assertNull(table.put(new CollidingKey("C"), "D"));
+        assertNull(table.put(new CollidingKey("E"), "F"));
+
+        assertEquals("B", table.remove(new CollidingKey("A")));
+        assertEquals("C", table.remove(new CollidingKey("B")));
+        assertEquals("D", table.remove(new CollidingKey("C")));
+        assertEquals("F", table.remove(new CollidingKey("E")));
+    }
+
+    @Test
+    public void test_size() {
+        Hashtable<Object, String> table = new Hashtable<>(5, 0.7f);
+        assertEquals(0, table.size());
+        assertNull(table.put("A", "B"));
+        assertEquals(1, table.size());
+        assertNull(table.put("B", "C"));
+        assertEquals(2, table.size());
+        assertNull(table.put("C", "D"));
+        assertEquals(3, table.size());
+        assertNull(table.put("E", "F"));
+        assertEquals(4, table.size());
+
+        assertNull(table.put(new CollidingKey("G"), "H"));
+        assertEquals(5, table.size());
+        assertNull(table.put(new CollidingKey("I"), "J"));
+        assertEquals(6, table.size());
+        assertNull(table.put(new CollidingKey("K"), "L"));
+        assertEquals(7, table.size());
+        assertNull(table.put(new CollidingKey("M"), "N"));
+        assertEquals(8, table.size());
+
+        table.remove("A");
+        assertEquals(7, table.size());
+        table.remove("B");
+        assertEquals(6, table.size());
+        table.remove("C");
+        assertEquals(5, table.size());
+        table.remove("E");
+        assertEquals(4, table.size());
+
+        table.remove(new CollidingKey("G"));
+        assertEquals(3, table.size());
+        table.remove(new CollidingKey("I"));
+        assertEquals(2, table.size());
+        table.remove(new CollidingKey("K"));
+        assertEquals(1, table.size());
+        table.remove(new CollidingKey("M"));
+        assertEquals(0, table.size());
+    }
+
+    // -------------------- Private Classes --------------------
+
+    private static final class CollidingKey {
+        private String value;
+
+        CollidingKey(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CollidingKey that = (CollidingKey) o;
+            return Objects.equals(value, that.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return 1;
+        }
     }
 }
